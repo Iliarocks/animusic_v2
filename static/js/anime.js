0449480fd2625144dd95c43b7226cf757535566b
Iliarocks <parun.ilia@gmail.com>
2020-07-17 21:37:18 -0400

    before reset

--- static/js/anime.js
+++ static/js/anime.js
@@ -1,11 +1,54 @@
 const database = firebase.database();
 const auth = firebase.auth();
 const storage = firebase.storage();
+var anime;
 
 //create error
 function error(message) {
   errorText.innerText = message;
   document.querySelector('.error').style.right = '30px';
+}
+
+function wikiEvent(url) {
+  document.querySelector('#open-wiki').onclick = () => {
+    window.location = url;
+  }
+}
+
+
+function loadSongs() {
+  database.ref(`/anime/${window.animeName}/songs`).once('value', snap => {
+    if (snap.val() === null) return;
+    let songs = Object.values(snap.val());
+    console.log(songs)
+    let newHTML = songs.reduce((html, song) => html + song.embeded, '');
+    document.querySelector('.song-holder').innerHTML = newHTML;
+  })
+}
+  
+function loadInfo() {
+  database.ref(`/anime/${anime}`).once('value', function(snap) {
+    if (snap.val() === null) return;
+    var animeObj = snap.val()
+    document.querySelector('#cover-image').style.backgroundImage = `url(${animeObj.cover_src})`;
+    document.querySelector('#en-name').innerText = animeObj.en_name;
+    document.querySelector('#jap-name').innerText = animeObj.jap_name;
+    wikiEvent(animeObj.wiki)
+  })
+}
+
+async function addSong() {
+  var url = document.querySelector('#spotify-input').value;
+  if (url.split('/').indexOf('album') > 0) return;
+  var embeded = `<iframe src="https://open.spotify.com/embed/track/${url.split('/')[url.split('/').length - 1].split('=')[0].split('?')[0]}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+  let exists = await songExists(embeded)
+  if (exists.length > 0) {
+    error('Hmm... seems like this song has already been added😁');
+    return;
+  }
+  database.ref(`anime/${window.animeName}/songs`).push({ embeded })
+  document.querySelector('.add-song-prompt').style.display = 'none';
+  document.querySelector('#spotify-input').value = '';
 }
 
 function signOut() {
@@ -22,6 +65,11 @@ auth.onAuthStateChanged(user => {
     document.querySelector('.signed-in-header').style.display = 'none';
     document.querySelector('.signed-out-header').style.display = 'inline-block';
   }
+})
+
+//close error
+closeErrorBtn.addEventListener('click', function(e) {
+  document.querySelector('.error').style.right = '-360px';
 })
 
 document.querySelector('header > div > h1').addEventListener('click', function() {
--- static/js/login.js
+++ static/js/login.js
@@ -6,6 +6,8 @@ const passIn = document.querySelector('#password-input');
 const actionBtn = document.querySelector('#action-btn');
 const linkBtn = document.querySelector('#link');
 const headerTitle = document.querySelector('header > h1');
+const closeErrorBtn = document.querySelector('#close-error');
+const errorText = document.querySelector('.error > p');
 
 auth.onAuthStateChanged(user => {
   if (user) window.location = window.origin;
@@ -13,25 +15,54 @@ auth.onAuthStateChanged(user => {
 
 if (window.action === 'Sign in') document.querySelector('#name-input').style.display = 'none';
 
+//create error
+function error(message) {
+  errorText.innerText = message;
+  document.querySelector('.error').style.right = '30px';
+}
+
+function nameExists(username) {
+  if (username === '') return;
+  return database.ref('/usernames').once('value').then(async function(snap) {
+    var exists = await Object.values(snap.val()).filter(name => name === username);
+    if (exists.length > 0) {
+      nameIn.style.borderBottom = '3px solid #e83333';
+      return true;
+    }
+    nameIn.style.borderBottom = '3px solid #42e833';
+    return false;
+  })
+}
+
 //sign user up
-function signUp() {
-  var name = nameIn.value;
+async function signUp() {
+  var username = nameIn.value.toLowerCase();
   var email = emailIn.value;
   var pass = passIn.value;
-  if (name === '' || email === '' || pass === '') return;
+  var exists = await nameExists(username);
+  if (exists || username === '') {error('Hmm... seems like the name is unavailable or badly formatted'); return;};
   auth.createUserWithEmailAndPassword(email, pass)
-    .catch(err => alert(err.message))
-    .then(() => database.ref(`users/${auth.currentUser.uid}`).set({ full_name: name }))
+    .then(() => {
+      database.ref(`users/${auth.currentUser.uid}`).set({ username, uid: auth.currentUser.uid})
+      database.ref(`/usernames`).update({ [auth.currentUser.uid]: username })
+    })
+    .catch(err => error(err.message))
 }
 
 //sign user in
 function signIn() {
   var email = emailIn.value;
   var pass = passIn.value;
-  if (email === '' || pass === '') return;
   auth.signInWithEmailAndPassword(email, pass)
-    .catch(err => alert(err.message))
+  .catch(err => {
+    error(err.message);
+    return;
+  })
 }
+
+nameIn.addEventListener('input', function(e) {
+  nameExists(e.target.value.toLowerCase())
+})
 
 //listen for click to either log user in or sign user up
 actionBtn.addEventListener('click', function(e) {
@@ -57,4 +88,9 @@ linkBtn.addEventListener('click', function(e) {
 //go home
 headerTitle.addEventListener('click', function(e) {
   window.location = window.origin;
+})
+
+//close error
+closeErrorBtn.addEventListener('click', function(e) {
+  document.querySelector('.error').style.right = '-360px';
 })
