a3d6db69b5da08d31cdb412137683b2dac5c5deb
Iliarocks <parun.ilia@gmail.com>
2020-08-13 20:19:21 -0400

    revert

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template
 
-from OpenSSL import SSL
-context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')
-
 app = Flask(__name__);
 
 @app.route('/')
